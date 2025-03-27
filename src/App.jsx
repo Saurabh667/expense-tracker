2f7ae5e05aea9059c4bf0f796df25469883851d2
Saurabh667 <saurav000667@gmail.com>
2025-03-27 12:53:53 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,19 @@ const DUMMY_EXPENSES = [
     id: 'e2',
     title: 'New TV',
     amount: 799.49,
-    date: new Date(2021, 2, 12)
+    date: new Date(2022, 2, 12)
   },
   {
     id: 'e3',
     title: 'Room-rent',
     amount: 2000,
-    date: new Date(2021, 2, 28),
+    date: new Date(2023, 2, 28),
   },
   {
     id: 'e4',
     title: 'Food-expenses',
     amount: 1500,
-    date: new Date(2021, 5, 12),
+    date: new Date(2023, 5, 12),
   },
 ];
 
